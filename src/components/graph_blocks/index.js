25f9b24b4339384a526796703eef5b4164f970ed
Maxim Eremin <maxtanner238@gmail.com>
2022-09-20 12:56:54 +0700

    переделал блоки под свг

--- src/components/graph_blocks/index.js
+++ src/components/graph_blocks/index.js
@@ -1,109 +1,140 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { ReactComponent as Graph_digital } from '../../imgs/digital_exiter.svg'
 import './index.css'
 
-const graph_blocks_map = [
-  {
-    modulator: [
-      {
-        block_header:'модулятор',
-        icon_type: '1',
-        param_list: [
-          {id:'0', label: 'Частота, МГц'},
-          {id:'1', label: 'Температура, С'},
-          {id:'2', label: 'Ротр, Вт'},
-          {id:'3', label: 'Сила тока 1, А'},
-        ],
-      }
-    ],
 
-    exiter: [
-      {
-        block_header:'усилитель',
-        icon_type: '1',
-        param_list: [
-          {id:'0', label: 'Напряжение, В'},
-          {id:'1', label: 'Сила тока, А'},
-        ],
-      }
-    ],
+const status_colors = [
+  '#658E9C', //hibernation
+  '#7adc47', //good
+  '#f9c22e', //warning
+  '#fe5f55', //error
+]
 
-    out: [
-      {
-        block_header:'выход',
-        icon_type: '1',
-        param_list: [
-          {id:'0', label: 'Рвых, Вт'},
-          {id:'1', label: 'Ротр, Вт'},
-          {id:'2', label: 'КСВ'},
-        ],
-      }
-    ]
-  },
-];
 
 function Status_graphs(props) {
-  const [graph_data, setGraphData] = React.useState({
-    data: {},
-  })
+  // const [graph_data, setGraphData] = React.useState({
+  //   data: {},
+  // })
+
+  const graph_ref = React.useRef(null)
 
   React.useEffect(() => {
-    let request_obj = {
-      name: 'status_graphs',
+    let graph_svg = graph_ref.current
+    let graph_data = props.data
+
+    for (const key in graph_data) {
+      let graph_block = graph_svg.querySelector(`#${key}`)
+      let graph_block_data = graph_data[key]
+
+      for (const item in graph_block_data) {
+        
+        if (item == "status") {
+          let device_icon = graph_block.querySelector(`#${key}_device_icon`)
+
+          device_icon.attributes.fill.value = status_colors[graph_block_data[item]]
+          
+          continue
+        }
+        
+        let graph_block_value_span_id = `#${key}_${item}_value`
+        let graph_block_value_span = graph_block.querySelector(graph_block_value_span_id).children[0]
+        
+        let new_value
+
+        if (Array.isArray(graph_block_data[item])) {
+          let input_value = graph_block_data[item][0],
+              divider = graph_block_data[item][1],
+              status = graph_block_data[item][2],
+              postfix = graph_block_data[item][3] ? 
+                        ' ' + graph_block_data[item][3] :
+                        ''                        ,
+
+              fract = (input_value / divider) % 1,
+              round_index
+
+          if (fract < 0.1 &&
+              fract > 0) {
+            round_index = 2
+          } else if (fract == 0) {
+            round_index = 0
+          } else {
+            round_index = 1
+          }
+
+          new_value = (input_value / divider).toFixed(round_index)
+          new_value = new_value + postfix
+
+          if (status != 1) {
+            graph_block_value_span.attributes["font-weight"].value = "500";
+            graph_block_value_span.style.fill = status_colors[status];
+          }
+
+        } else {
+          new_value = graph_block_data[item]
+        }
+
+        // if (item == "input_pwr" ||
+        //     item == "output_pwr") {
+        //   new_value = new_value + " Вт"
+        // }
+
+        console.log(graph_block_value_span);
+
+        graph_block_value_span.innerHTML = new_value
+      }
     }
 
-    let graph_timer = setInterval(() => {
-      console.dir(graph_blocks_map);
-      props.updateHandler(request_obj)
-    }, 10000)
 
-  }, [])
+    // let amplifier_amperage_value = graph_svg.querySelector('#amplifier_amperage1_value')
+    // console.dir(graph_ref.current);
+    // console.log(amplifier_amperage_value);
+    // amplifier_amperage_value.children[0].innerHTML = '9999';
+    
+  }, [props.data])
 
   return (
     <div className="graphs">
-      <Graph_block type="modulator" data="" />
-      <Graph_block type="exiter" data="" />
-      <Graph_block type="exiter" data="" />
-      <Graph_block type="out" data="" />
+      <Graph_digital ref={graph_ref} />
     </div>
   )
 }
 
-function Graph_block(props) {
-  return (
-    <div className="graph_container">
-      <div className="graph_header">
-        <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <rect width="14" height="14" rx="2" fill="#7ADC47"/>
-        </svg>
-        <h4>{graph_blocks_map[0][props.type][0].block_header}</h4>
-      </div>
-      <div className="graph_pwr_display">
-          <div className="pwr_out_display">
-            <div className="pwr_indicator_in"></div>
-            <span>120 Вт</span>
-          </div>
-          <div className="pwr_out_display">
-            <span>120 Вт</span>
-            <div className="pwr_indicator_out"></div>
-          </div>
-      </div>
-      <ul className="graph_main_params">
-        {graph_blocks_map[0][props.type][0].param_list.map(item => {
-          return (
-            <li
-              key={item.id}
-              id={item.id}
-              className="graph_item">
-              <span>{item.label}</span>
-              <span>0</span>
-            </li>
-          )
-        })}
-      </ul>
-    </div>
-  )
-}
+// function Graph_block(props) {
+//   return (
+//     <div className="graph_container">
+//       <div className="graph_header">
+//         <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+//           <rect width="14" height="14" rx="2" fill="#7ADC47"/>
+//         </svg>
+//         <h4>{graph_blocks_map[0][props.type][0].block_header}</h4>
+//       </div>
+//       <div className="graph_pwr_display">
+//           <div className="pwr_out_display">
+//             <div className="pwr_indicator_in"></div>
+//             <span>120 Вт</span>
+//           </div>
+//           <div className="pwr_out_display">
+//             <span>120 Вт</span>
+//             <div className="pwr_indicator_out"></div>
+//           </div>
+//       </div>
+//       <ul className="graph_main_params">
+//         {graph_blocks_map[0][props.type][0].param_list.map(item => {
+//           return (
+//             <li
+//               key={item.id}
+//               id={item.id}
+//               className="graph_item">
+//               <span>{item.label}</span>
+//               <span>0</span>
+//             </li>
+//           )
+//         })}
+//       </ul>
+//     </div>
+//   )
+// }
 
 export default Status_graphs;
