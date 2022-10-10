c715c711ee51313e0498b7d5079839593b02fd00
Maxim Eremin <maxtanner238@gmail.com>
2022-10-10 17:37:12 +0700

    добавил переключатель защиты калибровки

--- src/components/settings_block_calib/forms/serialnum_version_calib.js
+++ src/components/settings_block_calib/forms/serialnum_version_calib.js
@@ -6,6 +6,7 @@ import FormInput from '../../form_input';
 function SerialNumVersionCalibSettings(props) {
 
   const [serialNumVersionCalibState, setSerialNumVersionCalibState] = React.useState({
+    calib_defence: 1,
     device_serial_num: '',
     device_type: [1, 1]
   })
@@ -67,6 +68,24 @@ function SerialNumVersionCalibSettings(props) {
     }))
   }
 
+  const handleChange_defence = (event) => {
+    const target = event.target;
+    const value = target.checked;
+    const name = target.name.replace('_calib', '');
+
+    setSerialNumVersionCalibState(prevState => ({
+      ...prevState,
+      [name]: + value
+    }))
+
+    const request_obj = {
+      address: 'calib_serialNum_vesrion.cgi',
+      data: `${name}$${+ value}`
+    }
+
+    props.clickHandler(request_obj);
+  }
+
   const handleClick_save = (event) => {
     const target = event.target,
           name = target.name.replace('_calib', ''),
@@ -84,6 +103,26 @@ function SerialNumVersionCalibSettings(props) {
   return (
     <Settings_block_calib header={`ввод сер. номера и типа устр.`}
       settings_type={`voltage_primary_calib`}>
+      <li
+        key='calib_defence_calib'
+        id='calib_defence_calib'
+        className="settings_item">
+        <div className='item_header'>
+          <label
+            htmlFor={`calib_defence_calib_input`}
+            className="settings_itemLabel">
+            Выкл/вкл защиту от калибровки
+          </label>
+        </div>
+        <div className='item_input'>
+          <FormInput
+            id={`calib_defence_calib_input`}
+            name={`calib_defence_calib`}
+            changeHandler={handleChange_defence}
+            input_value={serialNumVersionCalibState.calib_defence}
+            type="switch" />
+        </div>
+      </li>
       <li
         key='device_serial_num_calib'
         id='device_serial_num_calib'
