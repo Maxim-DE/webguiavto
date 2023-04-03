e45363cd014bfbaab9cc96224aaf49a08b3b8ad7
Maxim Eremin <maxtanner238@gmail.com>
2023-04-03 17:12:52 +0700

    мелкие доработки

--- src/components/device_assets/st_250/index.js
+++ src/components/device_assets/st_250/index.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
+import useGlobalStore from '../../../logic/auth_store'
+
 import GeneralSettingsSection from './sections/general'
 import NetworkSettingsSection from './sections/network'
 import InfoSection from './sections/info'
+import CalibSection from './sections/calib'
 
 export default function DeviceWrap_ST250(props) {
   
   const updateHandler = (data_block) => {
     props.updateHandler(data_block)
   }
+
+  const [authGlobalState, authGlobalActions] = useGlobalStore()
 
   return (
     <>
@@ -24,6 +29,14 @@ export default function DeviceWrap_ST250(props) {
         updateHandler={updateHandler}
         section_data={props.section_data.info === null ? 'null' : props.section_data.info} 
         />
+      {authGlobalState.auth_access.calib &&
+        <CalibSection
+          section_name="calibration"
+          section_header="калибровка"
+          updateHandler={updateHandler}
+          section_data={props.calib_data === null ? 'null' : props.calib_data}
+          adc_data={props.adc_data} />
+      }
     </>
     
   )
