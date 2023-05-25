552ea194e9024e5d8c58cd32dd9b3e37c3a8068e
Maxim Eremin <maxtanner238@gmail.com>
2023-05-25 17:56:55 +0700

    добавил пороги балласта

--- src/components/settings_block_calib/forms/wattage_additional_calib_st.js
+++ src/components/settings_block_calib/forms/wattage_additional_calib_st.js
@@ -8,18 +8,22 @@ import useGlobalStore from '../../../logic/auth_store';
 import cloneDeep from 'lodash/cloneDeep';
 
 import { calib_state_conversion } from '../../../logic/calib_state_conversion';
+import { filter_obj } from '../../../logic/utilites';
 
 function WattageAdditionalCalibSettings_ST(props) {
   const [wattageAdditionalCalibState, setWattageAdditionalCalibState] = React.useState({
     input_power: '',
     ballast_1_avaliable: 0,
-    ballast_1: ''
+    ballast_1: '',
+    ballast_1_threshold_low: '',
+    ballast_1_threshold_high: ''
   })
 
   const [authGlobalState, authGlobalActions] = useGlobalStore()
 
   React.useEffect(() => {
-    if (Object.keys(props.calib_data).length != 0) {
+    if (props.calib_data != undefined && 
+        Object.keys(props.calib_data).length != 0) {
       let calib_state_copy = cloneDeep(wattageAdditionalCalibState)
 
       for (const key in props.calib_data) {
@@ -136,6 +140,41 @@ function WattageAdditionalCalibSettings_ST(props) {
     
   }
 
+  const handleClick_threshold_save = (event) => {
+    const target = event.target,
+          name = target.name.replace('_calib', ''),
+          threshold_data = filter_obj(wattageAdditionalCalibState, (key, value) => key.includes(name))
+
+    let data_string = '',
+        save_obj = {}
+
+    for (const key in threshold_data) {
+      const multipier = props.calib_data?.[key] ? props.calib_data[key][1] : 10,
+            value = wattageAdditionalCalibState[key] * multipier
+
+      data_string += `${key}$${value};`
+      save_obj[key] = [
+        value,
+        multipier
+      ]
+    }
+
+    const request_obj = {
+      address: 'calib_ballast.cgi',
+      data: data_string,
+      update_data: wattageAdditionalCalibState,
+      notifications: {
+        good: 'default',
+        bad: 'default'
+      },
+      save_data: {
+        calib_additional_power: save_obj
+      } 
+    }
+
+    props.clickHandler(request_obj);
+  }
+
   const handleClick_calib_zeros = (event) => {
     const target = event.target,
       name = target.name.replace('_zeros_calib', ''),
@@ -227,7 +266,7 @@ function WattageAdditionalCalibSettings_ST(props) {
           <label
             htmlFor={`ballast_1_calib_input`}
             className="settings_itemLabel">
-            Балласт 1, X.XX кВт
+            Балласт 1, Вт
           </label>
           {!!wattageAdditionalCalibState.ballast_1_avaliable &&
             <FormInput
@@ -264,6 +303,59 @@ function WattageAdditionalCalibSettings_ST(props) {
             onClick={handleClick_ballast} />
         </div>
       </li>
+      <li
+      key='I3_threshold'
+      id='I3_threshold'
+      className="settings_item calib">
+      <div className='item_header'>
+        <label
+          htmlFor={`I3_threshold_input`}
+          className="settings_itemLabel">
+          Порог балласта 1
+        </label>
+      </div>
+      <div className='item_input'>
+        <span className='item_adc_value'>
+          АЦП: {props.adc_data.ballast_1_threshold}
+        </span>
+        <div
+          className="text_range_container"
+          id={`ballast_1_threshold_low_input`}
+          name={`ballast_1_threshold_low`}>
+          <span>от</span>
+          <input
+            type="text"
+            className={`text_range ${
+              wattageAdditionalCalibState.ballast_1_avaliable ? '' : 'disabled_input'}`}
+            data-threshold="low"
+            name={`ballast_1_threshold_low`}
+            disabled={!wattageAdditionalCalibState.ballast_1_avaliable}
+            value={wattageAdditionalCalibState.ballast_1_threshold_low}
+            onChange={handleChange}
+          />
+          <span>до</span>
+          <input
+            type="text"
+            className={`text_range ${
+              wattageAdditionalCalibState.ballast_1_avaliable ? '' : 'disabled_input'}`}
+            data-threshold="high"
+            name={`ballast_1_threshold_high`}
+            disabled={!wattageAdditionalCalibState.ballast_1_avaliable}
+            value={wattageAdditionalCalibState.ballast_1_threshold_high}
+            onChange={handleChange}
+          />
+        </div>
+        <input
+          id={`ballast_1_threshold_save_calib`}
+          name={`ballast_1_threshold_calib`}
+          className={`button_input ${
+              wattageAdditionalCalibState.ballast_1_avaliable ? '' : 'disabled_input'}`}
+          type="button"
+          value={'Сохранить'}
+          disabled={!wattageAdditionalCalibState.ballast_1_avaliable}
+          onClick={handleClick_threshold_save} />
+      </div>
+    </li>
     </Settings_block_calib>
   )
 }
