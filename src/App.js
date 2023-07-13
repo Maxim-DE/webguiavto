b03e8cbb1c651f886578b0f02d5402bdc68a0ea2
Maxim Eremin <maxtanner238@gmail.com>
2023-07-13 13:07:40 +0700

    добавил комменты в App

--- src/App.js
+++ src/App.js
@@ -49,13 +49,10 @@ let debounceTimer;
 
 function App() {
 
+  // глобальное состояние ошибок
   const [errGlobalState, errGlobalActions] = useGlobalErrPool()
 
-  const [errPool, setErrPool] = React.useState({
-    total_err_count: 0,
-    status_err_count: 0
-  })
-
+  // очередь запросов
   const [requestPool, setRequestPool] = React.useState({
     state: 'active',
     pool: [],
@@ -63,8 +60,10 @@ function App() {
     delayed_pool: []
   })
   
+  // глобальное состояние авторизации
   const [authGlobalState, authGlobalActions] = useGlobalStore()
   
+  // данные раздела статуса
   const [statusData, setStatusData] = React.useState({
     status_info: null,
     status_graph: null,
@@ -78,6 +77,7 @@ function App() {
     calib_available: 0
   })
   
+  // данные разделов
   const [sectionData, setSectionData] = React.useState({
     status_settings: null,
     settings: null,
@@ -86,11 +86,12 @@ function App() {
     info: null,
   })
   
+  // данные бокового нижнего меню
   const [peripheralData, setPeripheralData] = React.useState({
     structure: [],
   })
   
-  
+  // состояние раздела калибровки
   const [calibState, setCalibState] = React.useState({
     isOpen: false,
     data: {},
@@ -98,14 +99,17 @@ function App() {
 
   const nav_ref = React.useRef()
   
+  // функция для обработки данных, полученных с сетевых запросов
   const outputData_assignment = (output_name, output_data, output_params) => {
 
+    // обработка данных о бокового нижнего меню
     if (output_name === 'peripheral_structure') {
       setPeripheralData(prevState => ({
         ...prevState,
         structure: output_data[`structure`],
       }))
 
+    // обработка данных статуса
     } else if (/^status/.test(output_name)) {
       let status_state_copy = statusData;
       for (const key in output_data) {
@@ -116,7 +120,8 @@ function App() {
       if (errGlobalState.status_err_count > 0) {
         errGlobalActions.err_erase('status')
       }
-
+    
+    // обработка данных картинки в разделе статуса
     } else if (/media\/status_graph/gi.test(output_name)) {
 
       let status_state_copy = statusData;
@@ -129,11 +134,13 @@ function App() {
         ...prevState,
         status_svg: output_obj,
       }))
-
+    
+    // обработка полного лога ошибок
     } else if (output_name === 'GetLogErrorFull') {
       if (typeof output_param == 'string' &&
           output_params.length == 0) return
 
+      // обработка пользовательского журнала
       if (Object.hasOwn(output_params, 'userlog')) {
         let output_data_copy = cloneDeep(output_data.userlog)
 
@@ -143,7 +150,8 @@ function App() {
             status_full_logs: set_logs_id(output_data_copy)
           }
         })
-
+      
+      // обработка системного журнала
       } else if (Object.hasOwn(output_params, 'syslog')) {
         setCalibState(prevState => ({
           ...prevState,
@@ -158,9 +166,7 @@ function App() {
       }
     } 
 
-    // else if (output_name === 'SysLog' ||
-    //            output_name === 'http_log') {} 
-
+    // обработка развернутых логов
     else if (output_name === 'get_expanded_log') {
       if (typeof output_param == 'string' &&
           output_params.length == 0) return
@@ -171,6 +177,7 @@ function App() {
         log_id = output_params.log_num
       }
 
+      // обработка развернутых логов польз. журнала
       if (Object.hasOwn(output_params, 'userlog')) {
         let log_data = JSON.parse(JSON.stringify(statusData.status_full_logs))
         let new_log_data = syslog_handle_expand(output_data, log_data, log_id)
@@ -180,6 +187,7 @@ function App() {
           status_full_logs: new_log_data
         }))
 
+      // обработка развернутых логов системного журнала
       } else if (Object.hasOwn(output_params, 'syslog')) {
         let log_data = cloneDeep(calibState.data.calib_misc.sys_log),
             new_log_data = syslog_handle_expand(output_data, log_data, log_id)
@@ -195,11 +203,14 @@ function App() {
           }
         }))
       }
+
+    // обработка информации о конфигурации
     } else if (output_name === 'get_conf_info') {
       if (output_params.length == 0) return
 
       let conf_name = output_params.name
       let conf_data = cloneDeep(sectionData.settings.conf_manage)
+      // Обновляем информацию о конфигурации
       let new_conf_data = add_info_to_conf(output_data, conf_data, conf_name)
 
       setSectionData(prevState => ({
@@ -209,28 +220,35 @@ function App() {
           conf_manage: new_conf_data
         }
       }))
+    
+    // обработка данных калибровки
     } else if (output_name === 'calibration') {
       setCalibState(prevState => ({
         ...prevState,
         data: output_data
       }))
-
+    
+    // очистка данных авторизации при выходе из аккаунта
     } else if (output_name === 'logout') {
       authGlobalActions.set_is_auth(false)
       authGlobalActions.set_user_id('')
       authGlobalActions.set_auth_level(0)
       
+    // обработка запросов, связанные с измением состояния калибровки
     } else if (/^calib_.*/gi.test(output_name)) {
+      // если запрос на калибровку нуля, то пропускаем его, иначе он собъет все данные
       if (output_name.includes('_zero')) return
 
       if (output_name === 'calib_conf_file') {
+        // Перезагружаем страницу после сброса настроек калибровки по умолчанию
         if (Object.hasOwn(output_params, 'factory_reset')) {
           reload_page()
         }
       }
 
+      // если запрос без данных на сохранение, то пропускаем, иначе сломается хранилище
       if (Object.keys(output_data).length == 0) return
-
+      // Обновляем данные авторизации после успешной авторизации
       if (output_name === 'calib_passw') {
         authGlobalActions.set_is_auth(true)
         authGlobalActions.set_user_id(output_params.login)
@@ -238,10 +256,13 @@ function App() {
         return
       }
 
+      // Обновляем данные калибровки при обычном запросе с форм калибровки
       setCalibState(prevState => ({
         ...prevState,
         data: output_data
       }))
+
+     // Оработка запросов, связанных с редактированием списка пользователей
     } else if (output_name === 'get_user_list' ||
                output_name === 'edit_user' ||
                output_name === 'delete_user' ||
@@ -257,7 +278,8 @@ function App() {
           }
         }
       }))
-
+    
+    // обработка запросов на обновление данных основных настроек
     } else if (output_name === 'status_settings' ||
                output_name === 'settings' ||
                output_name === 'network' ||
@@ -267,6 +289,7 @@ function App() {
 
       switch (output_name) {
         case 'info':
+          // Преобразуем тип устройства в строковый формат для отображения в интерфейсе (при наличии в ответе)
           if (Object.hasOwn(output_data.info_general, 'type')) {
             output_data_copy = type_device_toStr(output_data)
           } else {
@@ -280,12 +303,15 @@ function App() {
           break;
       }
 
+      // Обновляем разделы настроек и информации о статусе устройства
       setSectionData((prevState) => {
         return {
           ...prevState,
           [output_name]: output_data_copy,
         }
       })
+
+    // обработка запросов с формы настроек в разделе статуса
     } else if (output_name === 'transmitter') {
       setSectionData((prevState) => ({
         ...prevState,
@@ -293,6 +319,7 @@ function App() {
       }))
       
     } else if (output_name === 'reboot_device') {
+      // Блокируем отправку запросов и перезагружаем страницу после перезагрузки устройства
       setRequestPool(prevState => ({
         ...prevState,
         state: 'blocked'
@@ -305,22 +332,29 @@ function App() {
   }
 
   React.useEffect(() => {
+     // Очищаем таймер задержки и проверяем, есть ли запросы в очереди
     clearTimeout(debounceTimer);
     if (requestPool.pool.length > 0) {
+       // Если есть запросы, запускаем таймер задержки
       debounceTimer = setTimeout(() => {
+        // Функция для обработки очереди запросов
         const async_queue_processing = async (queue_arr) => {
+          // Очищаем очередь запросов
           setRequestPool(prevState => ({
             ...prevState,
             pool: []
           }))
           
+          // Отправляем запросы и получаем ответы
           let queue_resp = await async_Fetch_queue(queue_arr)
 
+          // Обрабатываем ответы на запросы
           for (let index = 0; index < queue_resp.length; index++) {
-            const req_queue_data = queue_arr[index]
-            const req_resp = queue_resp[index]
+            const req_queue_data = queue_arr[index] // данные, передаваемые вместе с запросом
+            const req_resp = queue_resp[index] // данные, полученные из ответа на запрос
             const resp_status = req_resp.status
 
+            // Обрабатываем уведомления
             if (req_queue_data.notifications) {
               switch (resp_status) {
                 case 'success':
@@ -377,7 +411,7 @@ function App() {
             }
 
 
-            
+            // Обрабатываем данные ответа на запрос
             const request_name = req_resp.name,
                   request_params = req_resp.params,
                   remote_data = typeof req_resp.data === 'object' ? 
@@ -398,6 +432,7 @@ function App() {
                   req_data = req_queue_data.save_data
                 }
                 
+                // копируем нужное состояние в зависимости от имени запроса
                 switch (request_name) {
                   case 'status':
                     state_copy = JSON.parse(JSON.stringify(statusData))
@@ -435,6 +470,7 @@ function App() {
                 req_data = {}
               }
               
+              // производим слияние данных с ответа и копией состояния
               new_state = merge(state_copy, req_data)
 
               outputData_assignment(request_name, new_state, request_params);
@@ -445,11 +481,6 @@ function App() {
 
         let queue = requestPool.pool
         async_queue_processing(queue)
-
-        // setRequestPool(prevState => ({
-        //   ...prevState,
-        //   pool: []
-        // }));
       }, 200)
     }
   }, [requestPool.pool])
@@ -461,12 +492,15 @@ function App() {
   }, [requestPool.state])
 
   React.useEffect(() => {
+    // Проверяем наличие информации в разделе
     if (sectionData.info) {
+      // Если информации о правах доступа нет, то устанавливаем уровень доступа 0(низший)
       if (!Object.hasOwn(sectionData.info, 'auth_info')) {
         authGlobalActions.set_auth_level(0)
         return
       } 
 
+      // Если информация о правах доступа есть, то устанавливаем уровень доступа и авторизацию
       const auth_info = sectionData.info.auth_info
       console.log(auth_info);
       authGlobalActions.set_auth_level(auth_info.auth_level)
@@ -482,6 +516,7 @@ function App() {
 
   React.useEffect(() => {
 
+    // Объект с запросом информации
     let request_obj = {
       address: `info.cgi`,
       notifications: {
@@ -492,11 +527,14 @@ function App() {
       },
     }
 
+    // Отправляем запрос информации
     handlePoolUpdate(request_obj);
 
   }, [])
 
   const handlePoolUpdate = (requestData) => {
+
+    // Обрабатываем запросы на управление очередью
     if (requestData.action) {
       switch (requestData.action) {
         case 'block_queue':
@@ -518,18 +556,21 @@ function App() {
       }
     }
 
+    // Добавляем запрос в очередь, если очередь активна
     if (requestPool.state == 'active') {
       setRequestPool(prevState => ({
         ...prevState,
         pool: prevState.pool.concat(requestData),
       })) 
     } 
+    // Если очередь заблокирована, выводим сообщение в консоль
     else if (requestPool.state == 'blocked') {
       console.log('its blocked');
     } 
   }
 
   const navRefUpdate = (nav_link_name) => {
+    // Обновляем ссылку на раздел в навигации и прокручиваем к ней страницу
     nav_ref.current = document.getElementById(`${nav_link_name}_section`)
     if (nav_ref.current != null) {
       nav_ref.current.scrollIntoView({ block: "center", behavior: "smooth" })
@@ -540,9 +581,11 @@ function App() {
 
   return (
     <>
+      {/* Контейнер с уведомлениями */}
       <ToastContainer
         transition={Zoom}
       />
+
       <div className="App">
         <nav>
           <div className="nav_header">
@@ -551,10 +594,12 @@ function App() {
             ОКБ АЛЬФА
             </a>
           </div>
+          {/* Компонент с ссылками на разделы */}
           <Links_list 
             updateHandler={navRefUpdate}
             calibaAvailable={statusData.calib_available} />
           <div className='nav_fillblock'></div>
+          {/* Компонент с боковым нижним меню */}
           {/* <PeripheralMenu
             updateHandler={handlePoolUpdate}
             structure={peripheralData.structure}
@@ -562,6 +607,7 @@ function App() {
         </nav>
         <div className='main_wrap'>
           <header>
+
             <h1>{sectionData.info ? `${sectionData.info.info_general.Type_Device} №${sectionData.info.info_general.serial_number}` :
                                     '...'}</h1>
             <CalibLogButton 
@@ -585,11 +631,12 @@ function App() {
                 err_count={errGlobalState.status_err_count}
                 err_pool_actions={errGlobalActions} /> 
             }
-
+            {/* Сообщение об отсутсвии конфигурации */}
             {sectionData.info && sectionData.info.info_general.type[0] == 250 &&
               <NoConf_placeholder />
             }
-
+            
+            {/* Обертка для разных типов устройств */}
             {authGlobalState.auth_access.settings &&
               <DeviceWrap_switch
                 device_type={sectionData.info ? sectionData.info.info_general.type : [0, 0]}
@@ -606,18 +653,24 @@ function App() {
 }
 
 function DeviceWrap_switch({device_type, ...props}) {
+
+  // Получаем тип устройства из таблицы имен и мощностей устройств
   const device_name = device_name_table[device_type[0]],
         device_power = device_power_table[device_type[1]],
         device_type_str = `${device_name}_${device_power}`
 
+  // Получаем состояние и действия для хранения ссылок на секции(разделы)
   const [sectionState, sectionActions] = useSectionStore()
 
+  // Используем хук useInView для отслеживания видимых элементов
   const observed_elements = useInView(sectionState.section_pool)
 
+  // Обновляем пул секций(разделов) в хранилище состояний при изменении отслеживаемых элементов
   React.useEffect(() => {
     sectionActions.refresh_intersection_pool(observed_elements)
   }, [observed_elements])
 
+  // Определяем тип устройства и возвращаем соответствующий компонент
   if (device_type_str === 'st_250' ||
       device_type_str === 'st_100') {
     return (
