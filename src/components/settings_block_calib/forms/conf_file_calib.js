0f2ab3d53c003ac5d0720abfd1964c5bcfa4af28
Maxim Eremin <maxtanner238@gmail.com>
2023-09-13 12:33:22 +0700

    переместил создание нов. конф по типу устройства

--- src/components/settings_block_calib/forms/conf_file_calib.js
+++ src/components/settings_block_calib/forms/conf_file_calib.js
@@ -48,7 +48,8 @@ export default function ConfFileCalib(props) {
   });
 
   const [confCalibState, setConfCalibState] = React.useState({
-    factory_reset_available: 1
+    factory_reset_available: 1,
+    device_conf_type: 0
   })
 
   const [isNewConfAlertOpen, setIsNewConfAlertOpen] = React.useState(false)
@@ -141,6 +142,17 @@ export default function ConfFileCalib(props) {
     props.clickHandler(request_obj)
   }, [isUploading])
 
+  const handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name.replace('_calib', '');
+
+    setConfCalibState(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
   const handleClick_save = (event) => {
     const target = event.target,
       name = target.name.replace('_calib', ''),
@@ -166,6 +178,22 @@ export default function ConfFileCalib(props) {
     if (hex_dropzone_ref.current.dropzone) {
       hex_dropzone_instance.current.removeAllFiles(true)
     }
+  }
+
+  const device_conf_create = () => {
+    const conf_type_value = confCalibState.device_conf_type
+
+    const request_obj = {
+      address: 'calib_super_admin_conf_file.cgi',
+      data: `create_type_conf$${conf_type_value}`,
+      // reducer: reducers.calibration_form,
+      notifications: {
+        good: 'default',
+        bad: 'default'
+      },
+    }
+
+    props.clickHandler(request_obj);
   }
 
   return (
@@ -200,7 +228,7 @@ export default function ConfFileCalib(props) {
         <li
           key='create_new_conf'
           id='create_new_conf'
-          className="settings_item">
+          className="settings_item calib">
           <div className='item_header'>
             <label
               htmlFor={`tcreate_new_conf_input`}
@@ -210,12 +238,31 @@ export default function ConfFileCalib(props) {
           </div>
           <div className='item_input'>
             <FormInput
-              id={`open_new_conf_alert_input`}
-              name={`open_new_conf_alert`}
+              id={`device_conf_type_calib`}
+              name={`device_conf_type_calib`}
+              type='select'
+              input_value={confCalibState.device_conf_type}
+              title='Тип устройства'
+              variants={[
+                'УРЦ-100/300',
+                'УРЦ-500',
+                'УРЦ-1000',
+                'УРЦ-2000',
+                'УСТ-050/100',
+                'УСТ-250',
+                'УСТ-500',
+                'РЦ-ХХХ',
+                'СТ-100',
+                'СТ-250',
+              ]}
+              changeHandler={handleChange} />
+            <FormInput
+              id={`device_conf_type_calib_save`}
+              name={`device_conf_type_calib`}
               clickHandler={(e) => {
                 setIsNewConfAlertOpen(true)
               }}
-              label='Создать'
+              label='Создать с выбр. типом'
               type="button" />
           </div>
         </li>
@@ -230,7 +277,7 @@ export default function ConfFileCalib(props) {
           <FormInput
             id={`create_new_conf_input`}
             name={`create_new_conf`}
-            clickHandler={handleClick_save}
+            clickHandler={device_conf_create}
             label='Да'
             type="button" />
           <FormInput
