0fef9498b613f9cf3d69efd916da332d517dff22
Maxim Eremin <maxtanner238@gmail.com>
2023-10-23 12:41:30 +0700

    фикс отображения отсутствия конф.

--- src/App.js
+++ src/App.js
@@ -303,7 +303,7 @@ function App() {
               isAuthComplete = {auth_store.is_auth}/>
           </header>
           <main>
-            {Object.keys(info_section_data).length > 0 && info_section_data.info_general.model != 250 &&
+            {Object.keys(info_section_data).length > 0 && info_section_data.info_general.model != 255 &&
               <StatusSection
                 section_name="status"
                 section_header="Статус"
@@ -312,8 +312,12 @@ function App() {
                  /> 
             }
             {/* Сообщение об отсутсвии конфигурации */}
-            {Object.keys(info_section_data).length > 0 && info_section_data.info_general.model == 250 &&
+            {Object.keys(info_section_data).length > 0 && info_section_data.info_general.model == 255 &&
+              <>
               <NoConf_placeholder />
+              <DeviceWrap_unknown
+                updateHandler={handlePoolUpdate} />
+              </>
             }
             
             {/* Обертка для разных типов устройств */}
@@ -325,7 +329,7 @@ function App() {
                 // section_data={sectionData}
                 // calib_data={calibState.data}
                 adc_data={status_section_data.calib_adc} />
-            }
+              }
           </main>
         </div>
       </div>
@@ -343,37 +347,29 @@ function DeviceWrap_switch({ device_type, device_type_name, ...props }) {
 
   // Используем хук useInView для отслеживания видимых элементов
   // const observed_elements = useInView(sectionState.section_pool)
-
+  
   // Обновляем пул секций(разделов) в хранилище состояний при изменении отслеживаемых элементов
   // React.useEffect(() => {
-  //   sectionActions.refresh_intersection_pool(observed_elements)
-  // }, [observed_elements])
-
-  // Определяем тип устройства и возвращаем соответствующий компонент
-  if (device_type !== 255) {
-    return (
-      <DeviceWrap_BlockControl
-        updateHandler={props.updateHandler}
-        // section_data={props.section_data}
-        // calib_data={props.calib_data}
-        adc_data={props.adc_data} />
-    )
-  } else {
-    return (
-      <DeviceWrap_unknown
-        updateHandler={props.updateHandler} />
-    )
+    //   sectionActions.refresh_intersection_pool(observed_elements)
+    // }, [observed_elements])
+    
+    // Определяем тип устройства и возвращаем соответствующий компонент
+    if (device_type === 255) {
+      return (
+        <DeviceWrap_unknown
+          updateHandler={props.updateHandler} />
+      )
+    } else {
+      return (
+        <DeviceWrap_BlockControl
+          updateHandler={props.updateHandler}
+          // section_data={props.section_data}
+          // calib_data={props.calib_data}
+          adc_data={props.adc_data} />
+      )
   }
-  
   // switch (device_type) {
   //   case  0:
-  //     return (
-  //       <DeviceWrap_BlockControl
-  //         updateHandler={props.updateHandler}
-  //         // section_data={props.section_data}
-  //         // calib_data={props.calib_data}
-  //         adc_data={props.adc_data} />
-  //     )
     
   //   case 1:
   //     return (
@@ -403,10 +399,6 @@ function DeviceWrap_switch({ device_type, device_type_name, ...props }) {
   //     )
     
   //   default:
-  //     return (
-  //       <DeviceWrap_unknown
-  //         updateHandler={props.updateHandler} />
-  //     )
   // }
 
   
