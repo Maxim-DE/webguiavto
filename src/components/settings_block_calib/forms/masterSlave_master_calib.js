edbc678129855d5c563028af1bddfe3e20b83027
Maxim Eremin <maxtanner238@gmail.com>
2023-10-26 15:44:24 +0700

    мелкие фиксы

--- src/components/settings_block_calib/forms/masterSlave_master_calib.js
+++ src/components/settings_block_calib/forms/masterSlave_master_calib.js
@@ -9,12 +9,21 @@ import { HiOutlineRefresh } from 'react-icons/hi'
 import { useSelector } from 'react-redux';
 import { reducers } from '../../../store/reducers/calib_forms_reducers';
 
+const baudrate_arr = [
+  '9600',
+  '19200',
+  '38400',
+  '57600',
+  '115200'
+]
+
+
 export default function Modbus_master_calib(props) {
-  const calibMasterSlave_store = useSelector((store) => store.globalStore.global_data.calib_state.data?.calib_masterSlave)
+  const calibMasterSlave_store = useSelector((store) => store.globalStore.global_data.calib_state.data?.calib_modbus)
 
   const [masterSlaveCalibState, setMasterSlaveCalibState] = React.useState({
     master_form_availiable: 1,
-    master_connection_speed: '',
+    master_baudrate: 0,
     master_req_period: '',
     master_timeout: '',
     device_list: {
@@ -75,8 +84,15 @@ export default function Modbus_master_calib(props) {
 
   const handleClick_save = (event) => {
     const target = event.target,
-      name = target.name.replace('_calib', ''),
+          name = target.name.replace('_calib', '')
+
+    let value
+
+    if (name === 'master_baudrate') {
+      value = baudrate_arr[masterSlaveCalibState[name]]
+    } else {
       value = masterSlaveCalibState[name]
+    }
 
     const request_obj = {
       address: 'calib_modbus_master.cgi',
@@ -140,35 +156,29 @@ export default function Modbus_master_calib(props) {
       disableHandler={handleFormDisable}
       settings_type={`genera_modbus_slave_calib`} >
       <li
-        key='master_connection_speed_calib'
-        id='master_connection_speed_calib'
+        key='master_baudrate_calib'
+        id='master_baudrate_calib'
         className="settings_item calib">
         <div className='item_header'>
           <label
-            htmlFor={`master_connection_speed_input`}
+            htmlFor={`master_baudrate_input`}
             className="settings_itemLabel">
             Скорость передачи
           </label>
         </div>
         <div className='item_input'>
           <FormInput
-            id={`master_connection_speed_input`}
-            name={`master_connection_speed`}
+            id={`master_baudrate_input`}
+            name={`master_baudrate`}
             class="calib_input"
             disabled={!masterSlaveCalibState.master_form_availiable}
             changeHandler={handleChange}
-            input_value={masterSlaveCalibState.master_connection_speed}
+            input_value={masterSlaveCalibState.master_baudrate}
             type="select"
-            variants={[
-              '9600',
-              '19200',
-              '38400',
-              '57600',
-              '115200'
-            ]} />
+            variants={baudrate_arr} />
           <FormInput
-            id={`master_connection_speed_save`}
-            name={`master_connection_speed`}
+            id={`master_baudrate_save`}
+            name={`master_baudrate`}
             disabled={!masterSlaveCalibState.master_form_availiable}
             clickHandler={handleClick_save}
             label='Сохранить'
