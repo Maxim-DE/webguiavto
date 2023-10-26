048294f8dc693a7c43b68ca9484199eab29118aa
Maxim Eremin <maxtanner238@gmail.com>
2023-10-26 13:06:12 +0700

    фикс изменения адр. строки при переходе между вкладками

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { createContext } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 
 import { store } from './store/store';
 import { Provider } from 'react-redux';
@@ -11,11 +11,11 @@ import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <MemoryRouter>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </MemoryRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
