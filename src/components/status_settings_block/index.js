8faad64753305254f1afb83ad1ddbb5b0c276f65
Maxim Eremin <maxtanner238@gmail.com>
2023-11-27 11:14:47 +0700

    поменял меню настроек в статусе

--- src/components/status_settings_block/index.js
+++ src/components/status_settings_block/index.js
@@ -3,8 +3,8 @@ import React from 'react';
 import './index.css'
 import FormInput from '../form_input';
 
-import { TbPlus } from 'react-icons/tb'
-import { TbMinus } from 'react-icons/tb'
+import { BiPlus, BiPlusCircle, BiMinus, BiMinusCircle } from "react-icons/bi";
+
 
 import Settings_block_calib from '../settings_block_calib';
 import { reducers } from '../../store/reducers/status_settings_reducers';
@@ -13,7 +13,7 @@ function Status_settings(props) {
 
   const [statusSettingsState, setStatusSettingsState] = React.useState({
     supply_on_setting: 0,
-    channel_setting: 0
+    channel_setting: 0,
   })
 
   const device_status = props.status_data ? props.status_data.device_status : 0,
@@ -102,13 +102,13 @@ function Status_settings(props) {
     const target = event.currentTarget,
       name = target.name
 
-    let target_data = name.split('_'),
-      action = target_data[0],
-      action_value = parseInt(target_data[2])
+    // let target_data = name.split('_'),
+    //   action = target_data[0],
+    //   action_value = parseInt(target_data[2])
 
-    console.log(target_data);
+    // console.log(target_data);
 
-    let output_string = `power_${action}$${action_value}`
+    let output_string = `power_${name}$0`
 
     const request_obj = {
       address: 'transmitter.cgi',
@@ -142,7 +142,7 @@ function Status_settings(props) {
 
   const channel_save_handleClick = (event) => { //сделать позже так, разделением
     const value = statusSettingsState.channel_setting;
-    const name = 'channel';
+    const name = event.target.name;
 
     const request_obj = {
       address: 'transmitter.cgi',
@@ -183,139 +183,130 @@ function Status_settings(props) {
             type="switch" />
         </div>
       </li>
-      {props.device_type > 3 ||
-       props.device_type > 3 &&
-        <>
-        <li
-          key='channel_setting'
-          id='channel_setting'
-          className="settings_item">
-          <div className='item_header'>
-            <label
-              htmlFor={`channel_setting_input`}
-              className="settings_itemLabel">
-              Изменение канала
-            </label>
-          </div>
-          <div className='item_input'>
-            <input
-              id={`channel_setting_input`}
-              name={`channel_setting`}
-              type="text"
-              className={`${device_locked ? 'disabled_input' : ''}`}
-              disabled={device_locked}
-              onChange={handleChange_channel}
-              value={statusSettingsState.channel_setting}
-              maxLength="2"
-              style={{ maxWidth: '35px', marginRight: '10px'}}
+      <li
+        key='channel_setting'
+        id='channel_setting'
+        className="settings_item">
+        <div className='item_header'>
+          <label
+            htmlFor={`channel_setting_input`}
+            className="settings_itemLabel">
+            Изменение канала
+          </label>
+        </div>
+        <div className='item_input'>
+          <input
+            id={`channel_setting_input`}
+            name={`channel_setting`}
+            type="text"
+            className={`${device_locked ? 'disabled_input' : ''}`}
+            disabled={device_locked}
+            onChange={handleChange_channel}
+            value={statusSettingsState.channel_setting}
+            maxLength="2"
+            style={{ maxWidth: '35px', marginRight: '10px'}}
+          />
+          <FormInput
+            id={`channel_save_input`}
+            name={`channel_save`}
+            label='Сохранить'
+            disabled={device_locked}
+            clickHandler={channel_save_handleClick}
+            type="button"
+          />
+          {/* <FormInput
+            id={`channel_setting_input`}
+            name={`channel_setting`}
+            changeHandler={handleChange}
+            input_value={statusSettingsState.channel_setting}
+            type="text" /> */}
+        </div>
+      </li>
+      <li className="group_divider"></li>
+      <li
+        key='power_setting'
+        id='power_setting'
+        className="settings_item">
+        <div className='item_header'>
+          <label
+            htmlFor={`power_setting_input`}
+            className="settings_itemLabel">
+            Изменение мощности
+          </label>
+        </div>
+        <div className='item_input'>
+          {/* <FormInput
+            id={`power_setting_input`}
+            name={`power_setting`}
+            changeHandler={handleChange}
+            input_value={statusSettingsState.power_setting}
+            statusHandler={setStatusSettingsState}
+            type="text_buttons" /> */}
+          
+          <button
+            className={`button_input plus_minus ${device_locked ? 'disabled_input' : ''}`}
+            name='minus_big_step'
+            disabled={device_locked}
+            onClick={(e) => {
+              plusMinusHandler(e)
+            }}
+            >
+            <BiMinusCircle />
+          </button>
+          <button
+            className={`button_input plus_minus ${device_locked ? 'disabled_input' : ''}`}
+            name='minus_small_step'
+            disabled={device_locked}
+            onClick={(e) => {
+              plusMinusHandler(e)
+            }}
+            >
+            <BiMinus />
+          </button>
+          <button
+            className={`button_input plus_minus ${device_locked ? 'disabled_input' : ''}`}
+            name='plus_small_step'
+            disabled={device_locked}
+            onClick={(e) => {
+              plusMinusHandler(e)
+            }}
+            >
+            <BiPlus />
+          </button>
+          <button
+            className={`button_input plus_minus ${device_locked ? 'disabled_input' : ''}`}
+            name='plus_big_step'
+            disabled={device_locked}
+            onClick={(e) => {
+              plusMinusHandler(e)
+            }}
+            >
+            <BiPlusCircle />
+          </button>
+        </div>
+      </li>
+      <li
+        key='status_save_power_setting'
+        id='status_save_power_setting'
+        className="settings_item nested_item">
+        <div className='item_header'>
+          <label
+            htmlFor={`status_save_power_input`}
+            className="settings_itemLabel">
+            Фиксация мощности
+          </label>
+        </div>
+        <div className='item_input'>
+          <FormInput
+            id={`status_save_power_input`}
+            name={`status_save_power`}
+            label='Фиксировать'
+            disabled={device_locked}
+            clickHandler={power_save_handleClick}
+            type="button"
             />
-            <FormInput
-              id={`channel_save_input`}
-              name={`channel_save`}
-              label='Сохранить'
-              disabled={device_locked}
-              clickHandler={channel_save_handleClick}
-              type="button"
-            />
-            {/* <FormInput
-              id={`channel_setting_input`}
-              name={`channel_setting`}
-              changeHandler={handleChange}
-              input_value={statusSettingsState.channel_setting}
-              type="text" /> */}
-          </div>
-        </li>
-        <li
-          key='power_setting'
-          id='power_setting'
-          className="settings_item">
-          <div className='item_header'>
-            <label
-              htmlFor={`power_setting_input`}
-              className="settings_itemLabel">
-              Изменение мощности
-            </label>
-          </div>
-          <div className='item_input'>
-
-            {/* <FormInput
-              id={`power_setting_input`}
-              name={`power_setting`}
-              changeHandler={handleChange}
-              input_value={statusSettingsState.power_setting}
-              statusHandler={setStatusSettingsState}
-              type="text_buttons" /> */}
-            
-            <button
-              className={`button_input plus_minus ${device_locked ? 'disabled_input' : ''}`}
-              name='minus_value_3'
-              disabled={device_locked}
-              onClick={(e) => {
-                plusMinusHandler(e)
-              }}
-              >
-              <TbMinus />
-              3
-            </button>
-            <button
-              className={`button_input plus_minus ${device_locked ? 'disabled_input' : ''}`}
-              name='minus_value_1'
-              disabled={device_locked}
-              onClick={(e) => {
-                plusMinusHandler(e)
-              }}
-              >
-              <TbMinus />
-              1
-            </button>
-            <button
-              className={`button_input plus_minus ${device_locked ? 'disabled_input' : ''}`}
-              name='plus_value_1'
-              disabled={device_locked}
-              onClick={(e) => {
-                plusMinusHandler(e)
-              }}
-              >
-              <TbPlus />
-              1
-            </button>
-            <button
-              className={`button_input plus_minus ${device_locked ? 'disabled_input' : ''}`}
-              name='plus_value_3'
-              disabled={device_locked}
-              onClick={(e) => {
-                plusMinusHandler(e)
-              }}
-              >
-              <TbPlus />
-              3
-            </button>
-          </div>
-        </li>
-        <li
-          key='status_save_power_setting'
-          id='status_save_power_setting'
-          className="settings_item nested_item">
-          <div className='item_header'>
-            <label
-              htmlFor={`status_save_power_input`}
-              className="settings_itemLabel">
-              Фиксация мощности
-            </label>
-          </div>
-          <div className='item_input'>
-            <FormInput
-              id={`status_save_power_input`}
-              name={`status_save_power`}
-              label='Фиксировать'
-              disabled={device_locked}
-              clickHandler={power_save_handleClick}
-              type="button"
-              />
-          </div>
-        </li>
-        </>
-      }
+        </div>
+      </li>
     </Settings_block_calib>
   )
 }
