10f98bf228dd4498b432846b25585e81b3462d33
Maxim Eremin <maxtanner238@gmail.com>
2023-11-27 10:55:07 +0700

    мелкое дополнение к компоненту селекта

--- src/components/form_input/index.js
+++ src/components/form_input/index.js
@@ -448,11 +448,13 @@ function FormInput(
       >
       {props.variants &&
         props.variants.map((item, index) => {
-          return (
-            <option 
-              key={index}
-              value={index}>{item}</option>
-          )
+          if (typeof item != 'undefined') {
+            return (
+              <option 
+                key={index}
+                value={index}>{item}</option>
+            ) 
+          }
         })
       }
         {/* <option value='1'>4564</option>
