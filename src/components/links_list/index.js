6692d35e33fd402800d4a29fa4d403f7060bc66d
Maxim Eremin <maxtanner238@gmail.com>
2023-12-04 16:39:23 +0700

    пока скрыл кнопки обновления инфы о разделе

--- src/components/links_list/index.js
+++ src/components/links_list/index.js
@@ -127,7 +127,8 @@ function Links_list(props) {
               onClick={(e) => {console.log('nav span');}}
               >{item.name}</div>
 
-              <button 
+              {/* ПОХЖЕ НУЖНО ВЕРНУТЬ И ПЕРЕДЕЛАТЬ */}
+              {/* <button 
                 className='refresh_section_button button_input' type="button"
                 title='Обновить данные раздела'
                 name={`refresh_section_${item.id}`}
@@ -136,7 +137,7 @@ function Links_list(props) {
                 <IoMdRefresh
                     color='#6D8EA0'
                     size='25px' />
-              </button>
+              </button> */}
 
               <div className="frontIcon_wrap">
                 <FaAngleRight
@@ -226,7 +227,8 @@ function CalibNavList({updateHandler, refreshHandler, setParentActive}) {
               onClick={(e) => {console.log('nav span');}}
               >{item.name}</div>
 
-                <button
+                {/* ПОХЖЕ НУЖНО ВЕРНУТЬ И ПЕРЕДЕЛАТЬ */}
+                {/* <button
                   className='refresh_section_button button_input' type="button"
                   name={`refresh_section_calibration`}
                   title='Обновить данные раздела'
@@ -234,7 +236,7 @@ function CalibNavList({updateHandler, refreshHandler, setParentActive}) {
                   <IoMdRefresh
                     color='#6D8EA0'
                     size='25px' />
-                </button>
+                </button> */}
 
               <div className="frontIcon_wrap">
                 <FaAngleRight
