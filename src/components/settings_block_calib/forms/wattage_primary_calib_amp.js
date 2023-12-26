b53d2a2d646a21237079ee56aca30e20b6ac50a4
Maxim Eremin <maxtanner238@gmail.com>
2023-12-26 16:12:40 +0700

    мелкий фикс

--- src/components/settings_block_calib/forms/wattage_primary_calib_amp.js
+++ src/components/settings_block_calib/forms/wattage_primary_calib_amp.js
@@ -132,7 +132,7 @@ function PowerCalibSettings_AMP(props) {
         </div>
         <div className='item_input'>
           <span className='item_adc_value'>
-            АЦП: {adcPower_store.output_power}
+            АЦП: {adcPower_store?.output_power}
           </span>
           <FormInput
             id={`output_power_calib_input`}
@@ -158,7 +158,7 @@ function PowerCalibSettings_AMP(props) {
             htmlFor={`refected_power_calib_input`}
             className="settings_itemLabel"
             style={{fontWeight: '400'}}>
-            АЦП <sub>отр. мощ.</sub>: {adcPower_store.refected_power}
+            АЦП <sub>отр. мощ.</sub>: {adcPower_store?.refected_power}
           </label>
         </div>
       </li>
@@ -196,7 +196,7 @@ function PowerCalibSettings_AMP(props) {
         </div>
         <div className='item_input'>
           <span className='item_adc_value'>
-            ЦАП: {adcPower_store.output_power_threshold}
+            ЦАП: {adcPower_store?.output_power_threshold}
           </span>
           <FormInput
             id={`output_power_threshold_calib_input`}
@@ -227,7 +227,7 @@ function PowerCalibSettings_AMP(props) {
         </div>
         <div className='item_input'>
           <span className='item_adc_value' title='АЦП(отр) -x%*АЦП(вых)'>
-            АЦП<sub>отр. после комп.</sub>: {adcPower_store.coupling_coeff}
+            АЦП<sub>отр. после комп.</sub>: {adcPower_store?.coupling_coeff}
           </span>
           <FormInput
             id={`сoupling_coeff_calib_input`}
