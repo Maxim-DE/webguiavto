6d859aa9b0b4219c1e42eb576a693f9b189b6512
Maxim Eremin <maxtanner238@gmail.com>
2024-01-30 11:19:32 +0700

    добавил систему валидации + тултип для нее + мелкие фиксы компонентов формы + фиксы отображения раскрывающихся инпутов

--- src/components/settings_block_wrap/index.js
+++ src/components/settings_block_wrap/index.js
@@ -5,17 +5,21 @@ import { GrPowerReset } from 'react-icons/gr'
 
 import './index.css'
 
-function SettingsBlockWrap({ settings_type, className = "", ...rest }) {
+function SettingsBlockWrap({ settings_type, className = "", disable_save = false, ...rest }) {
   return (
     <div
       className={`settings_block ${settings_type ? settings_type : ""}`}>
-      <form className="settings_container">
+      <form className="settings_container" onSubmit={(e) => e.preventDefault()}>
         <div className="settings_block_header">
           <h3>{rest.header}</h3>
           {rest.save_handler &&
             <button 
-              className='save_form_button'
-              onClick={rest.save_handler}
+              disabled={disable_save}
+              className={`save_form_button ${disable_save && 'disabled_input'}`}
+              onClick={(e) => { 
+                e.preventDefault();
+                rest.save_handler(e)
+              }}
               type='button'
               title='Сохранить' >
               <MdDone style={{ margin: "3px 0 0 0" }} />
---
+++ src/logic/validation/validators.js
@@ -0,0 +1,79 @@
+export const required = (
+    message = 'Обязательное поле'
+) => {
+    return async (value) => (value ? null : message);
+};
+
+export const hasCyrillicSymbols = (
+    message = 'Присутствует кириллица'
+) => {
+    const regexp = /[а-яё]/i
+    return async (value) => (regexp.test(value) ? message : null);
+};
+
+export const minLength = (minCharCount = 1) => {
+    if (minCharCount <= 0) {
+        throw new Error(-
+            `Валидатор minLength ожидает положительное минимальное значение длины строки, получил ${minCharCount}`
+        )
+    }
+
+    return async value =>
+        value.length >= minCharCount
+            ? null
+            : `Количество символов не должно быть меньше ${minCharCount}`
+}
+
+export const maxLength = (maxCharCount) => {
+    if (maxCharCount <= 0) {
+        throw new Error(
+            `Валидатор maxLength ожидает положительное ограничение длины строки, получил ${maxCharCount}`
+        )
+    }
+
+    return async value =>
+        value.length <= maxCharCount
+            ? null
+            : `Количество символов не должно быть больше ${maxCharCount}`
+}
+
+export const isIpAdress = (
+    message = 'Неверный формат IP-адреса'
+) => {
+    const regexp = /^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$/i
+    return async (value) => (regexp.test(value) ? null : message);
+};
+
+export const isMacAdress = (
+    message = 'Неверный формат MAC-адреса'
+) => {
+    const regexp = /^([0-9A-Fa-f]{2}[:-]){5}([0-9A-Fa-f]{2})|([0-9a-fA-F]{4}\\.[0-9a-fA-F]{4}\\.[0-9a-fA-F]{4})$/i
+    return async (value) => (regexp.test(value) ? null : message);
+};
+
+export const isHexNumber = (
+    message = 'Значение не является 16-ти разрядным числом'
+) => {
+    const regexp = /^0[xX][0-9a-fA-F]+/i
+    return async (value) => (regexp.test(value) ? null : message);
+};
+
+export const isInNumRange = (minVal = 0, maxVal = 100000) => {
+    return async value => {
+        const dec_value = value % 1 == 0 ? parseInt(value) : parseFloat(value),
+              dec_min = value % 1 == 0 ? parseInt(minVal) : parseFloat(minVal),
+              dec_max = value % 1 == 0 ? parseInt(maxVal) : parseFloat(maxVal)
+
+        console.log(value % 1);
+
+        if (isNaN(dec_value) || isNaN(dec_min) || isNaN(dec_max)) {
+            return null
+        }
+
+        return (dec_value >= dec_min &&
+               dec_value <= dec_max)
+               ? null
+               : `Значение должно быть в диапазоне: ${minVal} - ${maxVal}`
+    }
+
+}
