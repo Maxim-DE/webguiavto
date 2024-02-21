b5f280cdfe71097be46e0e913802ea0b10e918a4
Maxim Eremin <maxtanner238@gmail.com>
2024-02-21 13:51:49 +0700

    Сделаkкнопку в блоке для разработчика (создать уникальный код)

--- src/components/device_assets/radio_amp/sections/calib/developer.js
+++ src/components/device_assets/radio_amp/sections/calib/developer.js
@@ -31,7 +31,8 @@ export const DeveloperCalib = (props) => {
         clickHandler={handleClick} />
       <ConsoleOutputCalibSettings
         clickHandler={handleClick} />
-      <MiscDownloadCalib />
+      <MiscDownloadCalib
+        clickHandler={handleClick} />
     </SettingsSectionWrap>
   )
 }
