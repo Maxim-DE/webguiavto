b0bd0b42ba4e04a6ce504489e5c017e7ec797a13
Maxim Eremin <maxtanner238@gmail.com>
2024-02-28 17:02:44 +0700

    дополнил форму удаления данных в меню разработчика

--- src/components/settings_block_calib/forms/data_erase_calib.js
+++ src/components/settings_block_calib/forms/data_erase_calib.js
@@ -10,6 +10,25 @@ import { reducers } from '../../../store/reducers/calib_forms_reducers';
 import { useSelector } from 'react-redux';
 
 export default function DataEraseCalibSettings(props) {
+  const [eraseSectorState, setEraseSectorState] = React.useState({
+    init_sector: 0,
+    sector_num: 0,
+  })
+
+  const style_obj = {
+    maxWidth: '60px'
+  }
+
+  const handleChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    setEraseSectorState(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
 
   const handleClick_save = (event) => {
     const target = event.target,
@@ -28,6 +47,19 @@ export default function DataEraseCalibSettings(props) {
 
     props.clickHandler(request_obj);
 
+  }
+
+  const eraseSector_link_build = () => {
+    const request_obj = {
+      address: 'calib_data_erase.cgi',
+      data: `sector$${eraseSectorState.init_sector};num$${eraseSectorState.sector_num}`,
+      notifications: {
+        good: 'default',
+        bad: 'default'
+      },
+    }
+
+    props.clickHandler(request_obj);
   }
 
   return (
@@ -54,6 +86,82 @@ export default function DataEraseCalibSettings(props) {
             type="button" />
         </div>
       </li>
+      <li
+        key='sector_download'
+        id='sector_download'
+        className="settings_item">
+        <div className='item_header'>
+          <label
+            htmlFor={`sector_download_input`}
+            className="settings_itemLabel">
+            Удаление сектора
+          </label>
+        </div>
+        <div className='item_input'>
+        </div>
+      </li>
+      <li
+        key='init_sector'
+        id='init_sector'
+        className="settings_item nested_item">
+        <div className='item_header'>
+          <label
+            htmlFor={`init_sector_input`}
+            className="settings_itemLabel">
+            Начальный сектор
+          </label>
+        </div>
+        <div className='item_input'>
+          <input
+            name='init_sector'
+            type="text"
+            data-threshold="low"
+            style={style_obj}
+            value={eraseSectorState.init_sector}
+            onChange={handleChange}
+          />
+        </div>
+      </li>
+      <li
+        key='sector_num'
+        id='sector_num'
+        className="settings_item nested_item">
+        <div className='item_header'>
+          <label
+            htmlFor={`sector_num_input`}
+            className="settings_itemLabel">
+            Количество секторов
+          </label>
+        </div>
+        <div className='item_input'>
+          <input
+            name='sector_num'
+            type="text"
+            data-threshold="high"
+            style={style_obj}
+            value={setEraseSectorState.sector_num}
+            onChange={handleChange}
+          />
+        </div>
+      </li>
+      <li
+        key='sector_erase_actions'
+        id='sector_erase_actions'
+        className="settings_item">
+        <div className='item_header'>
+        </div>
+        <div className='item_input'>
+          <FormInput
+            id='sector_erase_input'
+            name='sector_erase'
+            clickHandler={eraseSector_link_build}
+            label='Удалить'
+            type="button">
+            Удалить
+          </FormInput>
+        </div>
+      </li>
+      <li className="group_divider"></li>
       <li
         key='conf_erase'
         id='conf_erase'
