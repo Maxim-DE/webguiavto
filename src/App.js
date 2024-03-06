0b77f019e492e85afee876704c04add88c7f9dcd
Maxim Eremin <maxtanner238@gmail.com>
2024-03-06 15:40:25 +0700

    поправил ссылку на наш сайт ОКБ

--- src/App.js
+++ src/App.js
@@ -289,7 +289,7 @@ function App() {
       <div className="App">
         <nav>
           <div className="nav_header">
-            <a href="okbalfa.ru/">
+            <a href="http://okbalfa.ru/">
             {/* <img src={logo} className="app_logo" /> */}
             ОКБ АЛЬФА
             </a>
