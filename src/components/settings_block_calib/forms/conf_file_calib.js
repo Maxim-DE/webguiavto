854ab39b04161da6784c543b0f5ffbd640c97829
Maxim Eremin <maxtanner238@gmail.com>
2024-04-18 16:07:25 +0700

    разблокировал кнопку загрузки прошивки на устр.

--- src/components/settings_block_calib/forms/conf_file_calib.js
+++ src/components/settings_block_calib/forms/conf_file_calib.js
@@ -441,8 +441,7 @@ export default function ConfFileCalib(props) {
             id="hex_upload_zone"
             name='file'
             ref={hex_dropzone_ref}
-            className={`button_input disabled_input`}
-            disabled={true}
+            className={`button_input`}
             type="button"
             value='Загрузить' />
         </div>
