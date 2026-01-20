dd6f98aa4f5d4482adef6e0122b664116fc24712
Maxim_DE <maxim@okbalfa.ru>
2026-01-20 12:51:50 +0700

    Исправляю ошибки сборки (частично поправил)

--- build_scripts/archive.sh
+++ build_scripts/archive.sh
@@ -1,3 +1,7 @@
-rm build_arch/*
-mkdir build_arch
-find build -type f | cpio -o > build_arch.cpio && mv build_arch.cpio build_arch
+if [ -d "build_arch" ]; then
+    rm -rf build_arch/*
+else
+    echo "Directory build_arch doesn't exist, creating..."
+fi
+mkdir -p build_arch
+find build -type f | cpio -o > build_arch.cpio && mv build_arch.cpio build_arch/
