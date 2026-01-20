a8c52750bbd17cddaef2bb844957eedd54b81a3e
Maxim_DE <maxim@okbalfa.ru>
2026-01-20 16:58:10 +0700

    Добавил, удаление *.map файлов

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,11 @@ let gzip_file_array = [
   'build/static/css/*.css',
   // 'build/static/media/*.svg',
   // 'build/static/media/*.png',
+  'build/static/js/*.js.map',    // Добавить
+  'build/static/css/*.css.map',  // Добавить  
+  'build/static/js/*.js.map.gz',    // Добавить
+  'build/static/css/*.css.map.gz',  // Добавить  
+
   'build/static/media/status_graph/*.svg'
 ];
 
