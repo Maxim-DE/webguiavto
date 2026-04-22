b230ca1b65f7a06ae362dce6740cbe14108b5b8c
Maxim_DE <maxim@okbalfa.ru>
2026-04-22 15:44:21 +0700

    Переименовал картинку, и удалил не нужные

--- src/components/status_section/index.js
+++ src/components/status_section/index.js
@@ -89,7 +89,7 @@ function StatusSection(props) {
 
       if (GRAPH_MODE_TEST) {
         let request_obj = {
-          address: 'static/media/status_graph/avr_1+1.svg',
+          address: 'static/media/status_graph/avr_100.svg',
           // address: 'static/media/status_graph/avr_1000.svg',
           type: 'text',
           options: 'localhost',
