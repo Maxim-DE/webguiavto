8210e8eb704af25877e0cff8ccf308ccd75ba9bc
fearsoem21 <fearsome21@icloud.com>
2026-06-04 09:16:52 +0700

    добавлена проверка светодиодов

--- src/components/settings_block_calib/forms/misc_developer_events.js
+++ src/components/settings_block_calib/forms/misc_developer_events.js
@@ -38,6 +38,29 @@ function MiscSendEvents(props) {
       [name]: value
     }))
   }
+    // Обработчик сохранения для переключателя светодиодов
+  const handleClick_saveLedSwitch = (event) => {
+    const target = event.target,
+          name = target.name.replace('_calib', ''),
+          value = LedSwitchState[name]
+
+    // Преобразуем для отправки на сервер: true (выкл) -> 0, false (вкл) -> 1
+    const dataValue = value ? 0 : 1;
+
+    const request_obj = {
+      address: 'DebugEventLed.cgi',  // замените на нужный адрес
+      data: `${name}$${dataValue}`,
+      notifications: {
+        good: 'default',
+        bad: 'default'
+      },
+      save_data: {
+        calib_led: LedSwitchState
+      }
+    }
+
+    props.clickHandler(request_obj);
+  }
 
   const file_download_download = (url) => {
     // const url = "mib/okb_alpha.mib"
@@ -148,7 +171,60 @@ const handleSendEvent = (transmitterId, command) => (event) => {
       </li>
 
       {/*  продолжение */}
-
+        <div className='item_input'>Проверка светодиодов</div>
+        {/* Переключатель для включения/выключения проверки светодиодов */}
+        <li
+          key='Led_enable'
+          id='Led_enable'
+          className="settings_item">
+          <div className='item_header'>
+            <label
+              htmlFor={`Led_enable_input`}
+              className="settings_itemLabel">
+              Включить проверку светодиодов  
+            </label>
+          </div>
+          <div className='item_input'>
+            <FormInput
+              id={`Led_enable_input`}
+              name={`Led_enable`}
+              changeHandler={(e) => {
+                handleChangeLed(e)
+                handleClick_saveLedSwitch(e)
+              }}
+              type="switch" 
+              input_value={LedSwitchState.Led_enable}/>
+          </div>
+        </li>
+        
+        <li
+          className="settings_item">
+          <div className='item_input'>
+            <FormInput
+              id={`Led_blue`}
+              name={`Led_blue`}
+              label='Синий'
+              clickHandler={handleSendEvent_Led(1)}
+              type="button" />
+          </div>
+          <div className='item_input'>
+            <FormInput
+              id={`Led_red`}
+              name={`Led_red`}
+              label='Красный'
+              clickHandler={handleSendEvent_Led(2)}
+              type="button" />
+          </div>
+          <div className='item_input'>
+            <FormInput
+              id={`Led_off`}
+              name={`Led_off`}
+              label='Выкл'
+              clickHandler={handleSendEvent_Led(3)}
+              type="button" />
+          </div>
+           
+        </li>        
       </>
       }
     </Settings_block_calib>
