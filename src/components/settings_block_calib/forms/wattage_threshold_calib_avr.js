29a9f77642e9579e7925ef80b7415a32e6a7fb42
fearsoem21 <fearsome21@icloud.com>
2026-07-16 11:12:13 +0700

    добавленно поле ввода для набора мощности временно расположено общие настройки -> установка порогов мощности

--- src/components/settings_block_calib/forms/wattage_threshold_calib_avr.js
+++ src/components/settings_block_calib/forms/wattage_threshold_calib_avr.js
@@ -91,6 +91,51 @@ function PowerCalibThreshold_AVR({ calib_state, clickHandler, ...props }) {
 
   }
 
+
+
+    const handleClick_save_2 = (event) => {
+    const target = event.target,
+          name = target.name.replace('_calib', '')
+
+    let value, state_to_save
+
+    if (calib_state != undefined && calib_state[name] != undefined) {
+      if (Array.isArray(calib_state[name])) {
+        if (typeof calib_state[name][1] == 'number' &&
+          calib_state[name][1] > 0) {
+          value = powerCalibState[name] * calib_state[name][1]
+        } else {
+          value = powerCalibState[name]
+        }
+        let state_obj = { [name]: value }
+        state_to_save = calib_state_conversion(state_obj, calib_state)
+      } else {
+        value = powerCalibState[name]
+        state_to_save = { [name]: value }
+      }
+    } else {
+      value = powerCalibState[name]
+      state_to_save = { [name]: value }
+    }
+
+    const request_obj = {
+      address: 'rated_power.cgi',
+      data: `${name}$${value}`,
+      reducer: reducers.save_avr_device_data,
+      notifications: {
+        good: 'default',
+        bad: 'default'
+      },
+      save_data: {
+        rated_power: state_to_save
+      }
+    }
+
+    clickHandler(request_obj);
+
+  }
+
+
   return (
     <Settings_block_calib header={`установка порогов мощности`}
                           settings_type={`power_threshold_calib`}
@@ -145,6 +190,33 @@ function PowerCalibThreshold_AVR({ calib_state, clickHandler, ...props }) {
             id={`power_timeout_reserved_calib_save`}
             name={`power_timeout_reserved_value_calib`}
             clickHandler={handleClick_save}
+            label='Сохранить'
+            type="button" />
+        </div>
+      </li>
+      <li
+        key='rated_power_calib'
+        id='rated_power_calib'
+        className="settings_item calib">
+        <div className='item_header'>
+          <label
+            htmlFor={`rated_power_calib_input`}
+            className="settings_itemLabel">
+            Номинальное значение мощности 
+          </label>
+        </div>
+        <div className='item_input'>
+          <FormInput
+            id={`rated_power_calib_input`}
+            name={`rated_power_value_calib`}
+            changeHandler={handleChange}
+            input_value={powerCalibState.rated_power_value}
+            style={{ margin: '0', maxWidth: '75px' }}
+            type="text" />
+          <FormInput
+            id={`rated_power_calib_save`}
+            name={`rated_power_value_calib`}
+            clickHandler={handleClick_save_2}
             label='Сохранить'
             type="button" />
         </div>
