e188f22b3adb09a9da37623ef2f96b793ef270b9
fearsoem21 <fearsome21@icloud.com>
2026-07-17 13:34:10 +0700

    Порог регулировки балласта

--- src/components/settings_block_calib/forms/wattage_additional_calib_bc.js
+++ src/components/settings_block_calib/forms/wattage_additional_calib_bc.js
@@ -30,7 +30,8 @@ function WattageAdditionalCalibSettings_BC(props) {
     ballast_1_avaliable: 0,
     ballast_1: '',
     ballast_1_threshold_low: '',
-    ballast_1_threshold_high: ''
+    ballast_1_threshold_high: '',
+    ballast_1_Limit: ''  // Добавлено новое поле
   })
 
   // store.globalStore.global_data.calib_state.data.calib_additional_power
@@ -75,9 +76,6 @@ function WattageAdditionalCalibSettings_BC(props) {
           value = wattageAdditionalCalibState[name],
           multipier = calibWattageAdditional_store?.[name] ? calibWattageAdditional_store[name][1] : 10
 
-    // let state_obj = { [name]: value },
-    //     converted_state = calib_state_conversion(state_obj, calibWattageAdditional_store)
-
     const converted_state = {
       [name]: [
         value * multipier,
@@ -109,8 +107,36 @@ function WattageAdditionalCalibSettings_BC(props) {
           value = wattageAdditionalCalibState[name],
           multipier = calibWattageAdditional_store?.[name] ? calibWattageAdditional_store[name][1] : 10
 
-    // let state_obj = { [name]: value },
-    //     converted_state = calib_state_conversion(state_obj, calibWattageAdditional_store)
+    const converted_state = {
+      [name]: [
+        value * multipier,
+        multipier
+      ]
+    }
+
+    const request_obj = {
+      address: 'calib_ballast.cgi',
+      data: `${name}$${value * multipier}`,
+      reducer: reducers.calibration_form,
+      update_data: wattageAdditionalCalibState,
+      notifications: {
+        good: 'default',
+        bad: 'default'
+      },
+      save_data: {
+        calib_additional_power: converted_state
+      } 
+    }
+
+    props.clickHandler(request_obj);
+  }
+
+  // Новый обработчик для сохранения коррекции АЧХ балласта
+  const handleClick_ballast_limit = (event) => {
+    const target = event.target,
+          name = target.name.replace('_calib', ''),
+          value = wattageAdditionalCalibState[name],
+          multipier = calibWattageAdditional_store?.[name] ? calibWattageAdditional_store[name][1] : 10
 
     const converted_state = {
       [name]: [
@@ -334,14 +360,14 @@ function WattageAdditionalCalibSettings_BC(props) {
         </label>
       </div>
       <div className='item_input'>
-        <span className='item_adc_value'>
+        {/* <span className='item_adc_value'>
           АЦП: {adcVoltage_store.ballast_1_threshold}
-        </span>
+        </span> */}
         <div
           className="text_range_container"
           id={`ballast_1_threshold_low_input`}
           name={`ballast_1_threshold_low`}>
-          <span>от</span>
+          от
           <FormInput
             type="text"
             class={`text_range`}
@@ -370,6 +396,37 @@ function WattageAdditionalCalibSettings_BC(props) {
           clickHandler={handleClick_threshold_save} />
       </div>
     </li>
+      <li
+        key='ballast_1_Limit_calib'
+        id='ballast_1_Limit_calib'
+        className="settings_item calib">
+        <div className='item_header'>
+          <label
+            htmlFor={`ballast_1_Limit_calib_input`}
+            className="settings_itemLabel">
+            Порог регулировки балласта
+          </label>
+        </div>
+        <div className='item_input'>
+          <FormInput
+            id={'ballast_1_Limit_calib_input'}
+            name={'ballast_1_Limit_calib'}
+            class={`calib_input`}
+            type="text"
+            disabled={!wattageAdditionalCalibState.ballast_1_avaliable}
+            changeHandler={handleChange}
+            input_value={wattageAdditionalCalibState.ballast_1_Limit}
+          />
+          <FormInput
+            id={`ballast_1_Limit_calib_save`}
+            name={`ballast_1_Limit_calib`}
+            type="button"
+            label='Сохранить'
+            value={'Сохранить'}
+            disabled={!wattageAdditionalCalibState.ballast_1_avaliable}
+            clickHandler={handleClick_ballast_limit} />
+        </div>
+      </li>
     </Settings_block_calib>
   )
 }
